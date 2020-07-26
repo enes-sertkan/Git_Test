e58d5946b83ddcdbfa0ee7816625faf1ba348169
enes-sertkan <68817773+enes-sertkan@users.noreply.github.com>
2020-07-26 19:20:34 +0200

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,6 +3,7 @@ import java.time.Clock;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den merhaba");
+         System.out.println("GitHub den merhaba");
 
     }
 }
